ffe62ec64d268f616211b6678e4439b08b765da6
Dmitry Soboliev <19leva89@gmail.com>
2025-05-28 11:25:20 +0300

    Setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,11 +6,31 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+	baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+	...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
+	{
+		parser: "@typescript-eslint/parser",
+		plugins: ["prettier"],
+		rules: {
+			"prettier/prettier": [
+				"error",
+				{
+					endOfLine: "auto",
+					printWidth: 110,
+					usePrettierrc: true,
+				},
+			],
+			"@typescript-eslint/no-explicit-any": "off",
+			"@typescript-eslint/no-unused-vars": "off",
+			"@typescript-eslint/no-unused-expressions": [
+				"error",
+				{ allowShortCircuit: true },
+			],
+		},
+	},
 ];
 
 export default eslintConfig;
