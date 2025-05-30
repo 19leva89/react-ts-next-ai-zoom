3bcc64e39447dcab3c103ef3c7c97fe073e22d3d
Dmitry Soboliev <19leva89@gmail.com>
2025-05-30 15:07:24 +0300

    add Agents Form

---
+++ src/modules/agents/ui/components/agents-list-header.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import { useState } from 'react'
+import { PlusIcon } from 'lucide-react'
+
+import { Button } from '@/components/ui'
+import { NewAgentDialog } from '@/modules/agents/ui/components/new-agent-dialog'
+
+export const AgentsListHeader = () => {
+	const [isDialogOpen, setIsDialogOpen] = useState<boolean>(false)
+
+	return (
+		<>
+			<div className="flex flex-col gap-y-4 py-4 px-4 md:px-8">
+				<div className="flex items-center justify-between">
+					<h5 className="font-medium text-xl">My agents</h5>
+
+					<Button onClick={() => setIsDialogOpen(true)}>
+						<PlusIcon />
+						New Agent
+					</Button>
+				</div>
+			</div>
+
+			<NewAgentDialog open={isDialogOpen} onOpenChange={setIsDialogOpen} />
+		</>
+	)
+}
